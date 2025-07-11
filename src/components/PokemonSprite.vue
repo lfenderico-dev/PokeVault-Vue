43b28bfe357f1449a0eba924e9cb91d529334aac
lfenderico-dev <lorenzofenderico.dev@gmail.com>
2025-07-11 19:40:02 +0200

    added basic styling for the sprite component

--- src/components/PokemonSprite.vue
+++ src/components/PokemonSprite.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <img v-if="pokemonFetched" :src="pokemonFetched.sprites.front_default" alt="pokemon sprite">
+  <div class="absolute top-80 m-20 ">
+    <img class="h-120 w-120"  v-if="pokemonFetched" :src="pokemonFetched.sprites.front_default" alt="pokemon sprite">
   </div>
 </template>
 
