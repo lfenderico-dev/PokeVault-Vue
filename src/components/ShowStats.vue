bf5d31ab3ca11be094b25b7a3817947c316a00a7
lfenderico-dev <lorenzofenderico.dev@gmail.com>
2025-07-11 23:39:08 +0200

    Added the stats in the showStats component in the MainPage

---
+++ src/components/ShowStats.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="pokemonFetched">
+    <ul>
+      <li><strong>Name:</strong> {{ pokemonFetched.name }}</li>
+      <li><strong>Types:</strong> {{ pokemonType }}</li>
+      <li><strong>Id:</strong> {{ pokemonFetched.id }}</li>
+      <li><strong>Height:</strong> {{ pokemonFetched.height }}m</li>
+      <li><strong>Weight:</strong> {{ pokemonFetched.weight }}kg</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+const props = defineProps(['pokemonFetched'])
+
+const pokemonType = computed(() => {
+   const pokemonTypes = props.pokemonFetched.types.map(x => x.type.name)
+   return pokemonTypes.join(', ')
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,12 +2,15 @@
   <div>
     <PokemonSearch @pokemon-fetched="handleData"/>
     <PokemonSprite :pokemonFetched/>
+    <ShowStats :pokemonFetched />
+
   </div>
 </template>
 
 <script setup lang="ts">
 import PokemonSearch from '@/components/PokemonSearch.vue'
 import PokemonSprite from '@/components/PokemonSprite.vue';
+import ShowStats from '@/components/ShowStats.vue';
 
 import { ref } from 'vue';
 const pokemonFetched = ref('')
